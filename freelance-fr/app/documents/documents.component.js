b8cd21cf5249f2570e9c730178ea0455fd07034a
kadenjenson <sobeit99@gmail.com>
2017-12-10 23:14:40 -0700

    Added cards for the docs page, added more mock data

--- freelance-fr/app/documents/documents.component.js
+++ freelance-fr/app/documents/documents.component.js
@@ -16,21 +16,21 @@ var DocumentsComponent = /** @class */ (function () {
                 description: 'asdfasdfasdf asdf',
                 file_url: 'http://google.com',
                 updated_at: '12/08/17',
-                image_url: 'http://google.com'
+                image_url: 'https://i.kinja-img.com/gawker-media/image/upload/t_original/1230310037952427074.jpg'
             },
             {
                 title: "The Second Doc",
                 description: 'asdfasdfasdf asdf',
                 file_url: 'http://google.com',
                 updated_at: '12/08/17',
-                image_url: 'http://google.com'
+                image_url: 'https://cdn.techjuice.pk/wp-content/uploads/2014/11/art-of-freelancing-1.jpg'
             },
             {
                 title: "The Third Doc",
                 description: 'asdfasdfasdf asdf',
                 file_url: 'http://google.com',
                 updated_at: '12/08/17',
-                image_url: 'http://google.com'
+                image_url: 'https://netdna.webdesignerdepot.com/uploads/2015/05/featured_freelance.jpg'
             }
         ];
     }
