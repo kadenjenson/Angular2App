42576e5904106421d53cc235552b0082daaa6bb9
kadenjenson <sobeit99@gmail.com>
2017-12-10 17:19:30 -0700

    Implement full dynamic proposal page and routes

--- freelance-fr/app/proposal/proposal-list.component.ts
+++ freelance-fr/app/proposal/proposal-list.component.ts
@@ -9,7 +9,7 @@ import { Proposal } from './proposal';
 export class ProposalListComponent {
   proposalOne: Proposal = new Proposal(14, "Kim Muller", "http://kadens-sick-website.com", "ruby on rails", 120, 90, 8, "client@email.com")
   proposalTwo: Proposal = new Proposal(21, "Google", "http://google.com", "java", 300, 200, 24, "client@email.com")
-  proposalThree: Proposal = new Proposal(6, "Wells Fargo", "http://wellsfargo.com", "angular", 212, 80, 13, "client@email.com")d
+  proposalThree: Proposal = new Proposal(6, "Wells Fargo", "http://wellsfargo.com", "angular", 212, 80, 13, "client@email.com")
 
   proposals: Proposal[] = [
     this.proposalOne, 
